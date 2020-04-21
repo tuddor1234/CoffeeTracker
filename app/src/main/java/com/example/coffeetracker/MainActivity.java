91118a4d1dbac15392023c8967417e35454cddfe
tudorTurdasan <56021930+tudorTurdasan@users.noreply.github.com>
2020-04-21 14:54:37 +0300

    First fragment almost done

--- app/src/main/java/com/example/coffeetracker/MainActivity.java
+++ app/src/main/java/com/example/coffeetracker/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.coffeetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.text.Layout;
+
+import com.example.coffeetracker.ui.CoffeePickerFragment;
+import com.example.coffeetracker.ui.StatisticsFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        CoffeePickerFragment cfp = new CoffeePickerFragment();
+//
+//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//
+//        ft.add(R.id.frame,cfp).commit();
+//
+
+        StatisticsFragment statisticsFragment = new StatisticsFragment();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        ft.add(R.id.frame,statisticsFragment).commit();
+
+
+
     }
 }
