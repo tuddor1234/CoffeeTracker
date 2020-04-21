ae7249576b87a2e222f11a794e64cb0b59a6435d
Tuns Adrian <tunsadrian99@gmail.com>
2020-04-21 16:35:39 +0300

    new project

--- app/src/main/java/com/example/coffeetracker/MainActivity.java
+++ app/src/main/java/com/example/coffeetracker/MainActivity.java
@@ -1,41 +1,30 @@
 package com.example.coffeetracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
-import android.text.Layout;
 
-import com.example.coffeetracker.ui.CoffeePickerFragment;
-import com.example.coffeetracker.ui.StatisticsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int frameIndex;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        CoffeePickerFragment cfp = new CoffeePickerFragment();
-//
-//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//
-//        ft.add(R.id.frame,cfp).commit();
-//
-
-        StatisticsFragment statisticsFragment = new StatisticsFragment();
-
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-
-        ft.add(R.id.frame,statisticsFragment).commit();
-
-
-
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
